d41694723a2df34b25017b4f646bbe3efb97707a
lixiang <good396975802@qq.com>
2016-12-13 18:57:08 +0800

    开始为接口准备

--- erp-provider/src/main/java/com/bbgkh/service/BaseService.java
+++ erp-provider/src/main/java/com/bbgkh/service/BaseService.java
@@ -1,10 +1,13 @@
 package com.bbgkh.service;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 /**
  * Created by lixiang on 2016/7/18.
  */
+@Repository
 public   interface BaseService<T> {
     public  void insert(T entity);
     public  List selectAll();
