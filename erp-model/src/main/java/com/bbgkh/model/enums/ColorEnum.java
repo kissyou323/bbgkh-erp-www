9b9df88781233d59b1946ffa7dfde006e7620652
lixiang <good396975802@qq.com>
2016-12-19 19:01:27 +0800

    添加了颜色的枚举类型

--- erp-model/src/main/java/com/bbgkh/model/enums/ColorEnum.java
+++ erp-model/src/main/java/com/bbgkh/model/enums/ColorEnum.java
@@ -3,5 +3,17 @@ package com.bbgkh.model.enums;
 /**
  * Created by lixiang on 12/19/2016.
  */
-public class ColorEnum {
+public enum  ColorEnum {
+
+    RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLO("黄色", 4);
+
+    private String name;
+
+    private int index;
+
+    private ColorEnum(String name, int index){
+        this.name=name;
+        this.index=index;
+    }
+
 }
